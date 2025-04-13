80000b91bcfbe280f8ff570379f3d9d4e2180d28
Orekhova <irinamarkina803@gmail.com>
2025-04-13 12:14:25 +0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/nnuu3a16qr6ujlja?svg=true)](https://ci.appveyor.com/project/IrinaOre/ahj7-frontend)
---
+++ src/libs/createRequest.js
@@ -0,0 +1,28 @@
+export default async function createRequest(options) {
+  const baseUrl = 'http://localhost:7070/?';
+  // const baseUrl = 'https://ahj7-backend1.onrender.com/?';
+
+  const { method, url, body } = options;
+
+  try {
+    const response = await fetch(baseUrl + url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (response.ok) {
+      if (response.status === 204) {
+        return { error: false, status: response.status };
+      }
+
+      return await response.json();
+    }
+
+    return { error: true, status: response.status };
+  } catch (err) {
+    return { error: true, status: 520 };
+  }
+}
