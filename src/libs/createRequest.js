56b6f8605665c3e9fcb17c1434c9fc1e3d5a0ea8
Orekhova <irinamarkina803@gmail.com>
2025-04-15 09:19:15 +0400

    url

--- src/libs/createRequest.js
+++ src/libs/createRequest.js
@@ -1,25 +1,11 @@
-// export default async function createRequest(options) {
-//   const baseUrl = 'http://localhost:7070/';
-//   // const baseUrl = 'https://hw-ahj7-http.onrender.com';
-
-//   const { method, url, body } = options;
-
-//   try {
-//     const response = await fetch(baseUrl + url, {
-//       method,
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(body),
-//     });
 export default async function createRequest(options) {
-  const url = 'http://localhost:7070';
+  const baseUrl = 'http://localhost:7070/?';
   // const baseUrl = 'https://hw-ahj7-http.onrender.com';
 
-  const { method, body } = options;
+  const { method, url, body } = options;
 
   try {
-    const response = await fetch(url, {
+    const response = await fetch(baseUrl + url, {
       method,
       headers: {
         'Content-Type': 'application/json',
