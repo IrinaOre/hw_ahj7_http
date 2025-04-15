8ca71998075339ed5f90cfc0967b4ae66312f729
Orekhova <irinamarkina803@gmail.com>
2025-04-15 09:00:24 +0400

    changes

--- src/libs/createRequest.js
+++ src/libs/createRequest.js
@@ -1,5 +1,5 @@
 export default async function createRequest(options) {
-  const baseUrl = 'http://localhost:7070/?';
+  const baseUrl = 'http://localhost:7070';
   // const baseUrl = 'https://hw-ahj7-http.onrender.com';
 
   const { method, url, body } = options;
